30cdb714c9291c667d4a41a53d666eecfcdcc5b0
Alexander Valdez <alex.valdezp22@gmail.com>
2023-08-06 09:56:21 -0500

    Working with API KAGGLE

---
+++ KAGGLE_API/kaggle-api.py
@@ -0,0 +1,21 @@
+# ESTE PROGRAMA PERMITE INTERACTUAR CON DATASET DE KAGGLE
+#--------------------------------------------------
+'''
+Developer : Alexander Valdez P.
+Ing. Electronico UNI
+'''
+import os
+import json
+# DEBEMOS IR A SETTING PARA OBTENER NUESTRA CLAVE
+api_token= {"username":"alexandervp","key":"ddd2ba4d2baa8c254b5dc218d86b62ba"}
+
+cmd = 'mkdir /c/Users/soporte/.kaggle'
+os.system(cmd)
+
+with open("C:/Users/soporte/.kaggle/kaggle.json",'w') as file:
+     json.dump(api_token,file)
+
+
+cmd = 'chmod 600 /c/Users/soporte/.kaggle/kaggle.json'
+os.system(cmd)
+print("Well Done!")
