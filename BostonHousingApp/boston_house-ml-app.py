95fbefafb13922ce720b4c5b5f4cae5b9ca76db6
Alexander Valdez <alex.valdezp22@gmail.com>
2023-08-27 17:36:26 -0500

    Final Update Web App Boston House Prices

--- BostonHousingApp/boston_house-ml-app.py
+++ BostonHousingApp/boston_house-ml-app.py
@@ -2,7 +2,9 @@ import streamlit as st
 import pandas as pd
 
 from sklearn import datasets
+from sklearn.ensemble import RandomForestRegressor
 import  matplotlib.pyplot as plt
+import shap
 
 st.write("""
 # Aplicacion Web Precio de Casas de Boston
@@ -18,7 +20,10 @@ print(boston.columns)
 features=['crim', 'zn', 'indus', 'chas', 'nox', 'rm', 'age', 'dis', 'rad', 'tax','ptratio', 'b', 'lstat']
 #X = boston.loc[:,features].values
 #Y = boston.loc[:,['medv']].values
-X=boston
+#print("xshape",type(X),X.shape)
+#print("yshape",type(Y),Y.shape)
+X=boston[features]
+Y=boston['medv']
 ## SIDEBAR
 # HEADER PARA ESPECIFICAR PARAMETROS DE ENTRADA
 st.sidebar.header("Especifica los Parametros de Entrada")
@@ -37,19 +42,19 @@ def user_input_features():
     PTRATIO = st.sidebar.slider('PTRATIO',X.ptratio.min(),X.ptratio.max(),X.ptratio.mean())
     B       = st.sidebar.slider('B',X.b.min(),X.b.max(),X.b.mean())
     LSTAT   = st.sidebar.slider('LSTAT',X.lstat.min(),X.lstat.max(),X.lstat.mean())
-    data    = {'CRIM':CRIM,
-                'ZN':ZN,
-                 'INDUS':INDUS,
-                  'CHAS':CHAS,
-                    'NOX':NOX,
-                      'RM':RM,
-                       'AGE':AGE,
-                        'DIS':DIS,
-                         'RAD':RAD,
-                          'TAX':TAX,
-                           'PTRATIO':PTRATIO,
-                            'B':B,
-                              'LSTAT':LSTAT}
+    data    = {'crim':CRIM,
+                'zn':ZN,
+                 'indus':INDUS,
+                  'chas':CHAS,
+                    'nox':NOX,
+                      'rm':RM,
+                       'age':AGE,
+                        'dis':DIS,
+                         'rad':RAD,
+                          'tax':TAX,
+                           'ptratio':PTRATIO,
+                            'b':B,
+                              'lstat':LSTAT}
     features = pd.DataFrame(data,index=[0])
     return features
 
@@ -61,3 +66,29 @@ st.write(df)
 st.write('---')
 
 # BUILD REGRESSION MODEL
+model = RandomForestRegressor()
+model.fit(X,Y)
+# APPLY MODEL TO MAKE PREDICTION
+prediction= model.predict(df)
+
+st.header("Prediction of MEDV")
+st.write(prediction)
+st.write('---')
+## EXPLICACION DEL MODELO DE PREDICTION USANDO SHAP VALUES
+##  https://github.com/slundberg/shap
+explainer = shap.TreeExplainer(model)
+shap_values = explainer.shap_values(X)
+
+st.header('Feature Importante')
+plt.title('Feature Importante based on SHAP values')
+shap.summary_plot(shap_values,X)
+st.pyplot(bbox_inches='tight')
+st.set_option('deprecation.showPyplotGlobalUse', False)
+st.write('---')
+
+
+
+plt.title('Feature Importance based on SHAP values(Bar)')
+shap.summary_plot(shap_values,X,plot_type='bar')
+st.pyplot(bbox_inches='tight')
+
